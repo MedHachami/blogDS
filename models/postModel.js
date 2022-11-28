ba09232a5c7402065da37abb188523391b69bf98
mohamed hachami <moha.hach.9@gmail.com>
2022-11-28 09:24:02 +0100

    create schema model

--- models/postModel.js
+++ models/postModel.js
@@ -4,18 +4,29 @@ const mongoose=require("mongoose")
 // definition d'un schèma
 const postSchema=mongoose.Schema({
     titre: {
-       // chaine de caractère unique non nulle
-    },
-    auteur:{
-        // chaine de caractère avec la valeur "unkown" par defaut
-    },
-    resume:{
-        // chaine de caractère non nulle de moins de 100 caractères
-    },
-
-    content: {
-        // chaine de caractère non nulle, au moins de 100 caractères
-    }
+        // chaine de caractère unique non nulle
+         type: String,
+         required: true,
+         unique: true
+     },
+     auteur:{
+         // chaine de caractère avec la valeur "unkown" par defaut
+         type: String,
+         default: "unkown"
+     },
+     resume:{
+         // chaine de caractère non nulle de moins de 100 caractères
+         type: String,
+         required: true,
+         //maxlength: 100
+     },
+ 
+     content: {
+         // chaine de caractère non nulle, au moins de 100 caractères
+         type: String,
+         required: true,
+         //minlength: 100
+     }
 }, {timestamps:true})
 
 //créer et exporter un Modéle Post
